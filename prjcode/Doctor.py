789eff97bc95d16e96a24f5647103dfcbc426ca2
Stephen Larsen <sdlarsen@ualberta.ca>
2016-10-27 16:28:14 -0600

    fixed indent errors

--- prjcode/Doctor.py
+++ prjcode/Doctor.py
@@ -128,100 +128,102 @@ def getPatientChartInfo(cursor):  # doctor 1, nurse 3
 
 def recordSymptom(cursor, conn, staff_id):  # doctor 2, nurse 4
 
-	cursor.execute('''
-		select hcno, chart_id
-		from charts
-		where edate is Null;
-		''')
+    cursor.execute('''
+    		select hcno, chart_id
+    		from charts
+    		where edate is Null;
+    		''')
 
-	patientCharts = cursor.fetchall()
+    patientCharts = cursor.fetchall()
 
-	chartNotSelected = True
-	patientNotSelected = True
-	while patientNotSelected:
-		hcno = raw_input("Enter Patient Health Care Number >")
-		for row in patientCharts:
-			if hcno == row[0]:
-				patientNotSelected = False
-				break
+    chartNotSelected = True
+    patientNotSelected = True
+    while patientNotSelected:
+        hcno = raw_input("Enter Patient Health Care Number >")
+        for row in patientCharts:
+            if hcno == row[0]:
+                patientNotSelected = False
+                break
         if patientNotSelected:
-            print "Patient #%s does not have an open chart" %hcno
+            print "Patient #%s does not have an open chart" % hcno
+
             choice = raw_input("Enter 'quit' to exit task or enter anything to try another Health care number >").lower()
             if choice == 'quit':
                 return False
 
-    while chartNotSelected:
-		chart_id = raw_input("Enter Patients Chart Number >")
 
-		if (hcno, chart_id) in patientCharts:
-			chartNotSelected = False
-		else:
-			print "Patient #%s does not have a chart #%s that is open" %(hcno,chart_id)
+    while chartNotSelected:
+        chart_id = raw_input("Enter Patients Chart Number >")
+
+        if (hcno, chart_id) in patientCharts:
+            chartNotSelected = False
+        else:
+            print "Patient #%s does not have a chart #%s that is open" % (hcno, chart_id)
             choice = raw_input("Enter 'quit' to exit task or enter anything to try another chart number >").lower()
             if choice == 'quit':
                 return False
 
     symptom = raw_input("Enter Patient Symptom >")
 
-	cursor.execute('''
-		insert into symptoms values (?,?,?,datetime('now'),?);
-		''', (hcno, chart_id, staff_id, symptom, ))
+    cursor.execute('''
+    		insert into symptoms values (?,?,?,datetime('now'),?);
+    		''', (hcno, chart_id, staff_id, symptom,))
 
-	conn.commit()
+    conn.commit()
 
 
 def recordDiagnosis(cursor, conn, staff_id):  # doctor 3
 
-	cursor.execute('''
-		select hcno, chart_id
-		from charts
-		where edate is Null;
-		''')
+    cursor.execute('''
+        select hcno, chart_id
+        from charts
+        where edate is Null;
+        ''')
 
-	patientCharts = cursor.fetchall()
+    patientCharts = cursor.fetchall()
 
-	chartNotSelected = True
-	patientNotSelected = True
-	while patientNotSelected:
-		hcno = raw_input("Enter Patient Health Care Number >")
-		for row in patientCharts:
-			if hcno == row[0]:
-				patientNotSelected = False
-				break
-		if patientNotSelected:
-			print "Patient #%s does not have an open chart" %hcno
+    chartNotSelected = True
+    patientNotSelected = True
+    while patientNotSelected:
+        hcno = raw_input("Enter Patient Health Care Number >")
+        for row in patientCharts:
+            if hcno == row[0]:
+                patientNotSelected = False
+                break
+            if patientNotSelected:
+                print "Patient #%s does not have an open chart" %hcno
 
-            choice = raw_input("Enter 'quit' to exit task or enter anything to try another Health care number >").lower()
-            if choice == 'quit':
-                return False
+                choice = raw_input("Enter 'quit' to exit task or enter anything to try another Health care number >").lower()
+                if choice == 'quit':
+                    return False
 
     while chartNotSelected:
-		chart_id = raw_input("Enter Patients Chart Number >")
+        chart_id = raw_input("Enter Patients Chart Number >")
 
-		if (hcno,chart_id) in patientCharts:
-			chartNotSelected = False
-		else:
-			print "Patient #%s does not have a chart #%s that is open" %(hcno,chart_id)
+        if (hcno,chart_id) in patientCharts:
+            chartNotSelected = False
+        else:
+            print "Patient #%s does not have a chart #%s that is open" %(hcno, chart_id)
             choice = raw_input("Enter 'quit' to exit task or enter anything to try another chart number >").lower()
             if choice == 'quit':
                 return False
 
     diagnosis = raw_input("Enter Diagnosis >")
 
-	cursor.execute('''
-		insert into diagnoses values (?,?,?,datetime('now'),?);
-		''', (hcno, chart_id, staff_id, diagnosis, ))
+    cursor.execute('''
+      insert into diagnoses values (?,?,?,datetime('now'),?);
+      ''', (hcno, chart_id, staff_id, diagnosis, ))
 
-	conn.commit()
+    conn.commit()
 
 
 def recordMedication(cursor, conn, staff_id): # doctor 4
 
     cursor.execute('''
-		select hcno, chart_id
-		from charts
-		where edate is Null;
-		''')
+        select hcno, chart_id
+        from charts
+        where edate is Null;
+        ''')
 
     patientCharts = cursor.fetchall()
 
@@ -240,12 +242,12 @@ def recordMedication(cursor, conn, staff_id): # doctor 4
                 return False
 
     while chartNotSelected:
-		chart_id = raw_input("Enter Patients Chart Number >")
+        chart_id = raw_input("Enter Patients Chart Number >")
 
-		if (hcno,chart_id) in patientCharts:
-			chartNotSelected = False
-		else:
-			print "Patient #%s does not have a chart #%s that is open" %(hcno,chart_id)
+        if (hcno,chart_id) in patientCharts:
+            chartNotSelected = False
+        else:
+            print "Patient #%s does not have a chart #%s that is open" %(hcno,chart_id)
             choice = raw_input("Enter 'quit' to exit task or enter anything to try another chart number >").lower()
             if choice == 'quit':
                 return False
@@ -253,68 +255,68 @@ def recordMedication(cursor, conn, staff_id): # doctor 4
     medication = raw_input("Enter Drug Name >").lower()
 
 
-	cursor.execute('''
-		select lower(drug_name)
-		from reportedallergies
-		where hcno = ?
-		;
-		''', (hcno,))
+    cursor.execute('''
+        select lower(drug_name)
+        from reportedallergies
+        where hcno = ?
+        ;
+        ''', (hcno,))
 
-	directAllergies = cursor.fetchall()
+    directAllergies = cursor.fetchall()
 
-	cursor.execute('''
-		select lower(r.drug_name), lower(i.canbe_alg)
-		from reportedallergies r, inferredallergies i
-		where r.hcno = ?
-		and r.drug_name = i.alg
-		''', (hcno,))
+    cursor.execute('''
+        select lower(r.drug_name), lower(i.canbe_alg)
+        from reportedallergies r, inferredallergies i
+        where r.hcno = ?
+        and r.drug_name = i.alg
+        ''', (hcno,))
 
-	inferredAllergies = cursor.fetchall()
+    inferredAllergies = cursor.fetchall()
 
-	if medication in directAllergies:
-		print "Warning Patient is allergic to %s" %medication
-		# override = raw_input("Do you wish to procede")
+    if medication in directAllergies:
+        print "Warning Patient is allergic to %s" %medication
+        # override = raw_input("Do you wish to procede")
 
-	for row in inferredAllergies:
-		if medication == row[1]:
-			print "Warning Patient is allergic to %s and therefore could be allergic to %s" %(row[0], row[1])
-			# maybe select a new med
-			break
-
-
-
-	cursor.execute('''
-		select d.sug_amount
-		from dosage d, patients p
-		where p.hcno = ?
-		and lower(d.drug_name) = ?
-		and d.age_group = p.age_group;
-		''', (hcno,medication,))
-
-	sugestedDosage = cursor.fetchall()
-
-	while 1:
-		amount = raw_input("Enter dosage amount >")
-
-		if amount > str(sugestedDosage[0][0]):
-			print "Amount entered is greater then the recommended dosage of %s for this Patients age group" %sugestedDosage[0][0]
-			procede = raw_input("continue with this dosage(y) or enter new dosage(n) >").lower()
-
-			if procede == 'y':
-				break
-			else:
-				continue
-		else:
-			break
+    for row in inferredAllergies:
+        if medication == row[1]:
+            print "Warning Patient is allergic to %s and therefore could be allergic to %s" %(row[0], row[1])
+            # maybe select a new med
+            break
 
 
-	numOfDays = raw_input("Enter the number of day for the medication >")
 
-	daymodifer = str('+'+str(numOfDays)+' days')
+    cursor.execute('''
+        select d.sug_amount
+        from dosage d, patients p
+        where p.hcno = ?
+        and lower(d.drug_name) = ?
+        and d.age_group = p.age_group;
+        ''', (hcno,medication,))
 
-	# (hcno, chart_id, staff_id, mdate, start_med, end_med, amount, drug_name)
-	cursor.execute('''
-		insert into medications values (?,?,?,datetime('now'),datetime('now'),datetime('now', ?),?,?);
-		''', (hcno, chart_id, staff_id, daymodifer, amount, medication, ))
-	conn.commit()
+    sugestedDosage = cursor.fetchall()
+
+    while 1:
+        amount = raw_input("Enter dosage amount >")
+
+        if amount > str(sugestedDosage[0][0]):
+            print "Amount entered is greater then the recommended dosage of %s for this Patients age group" %sugestedDosage[0][0]
+            procede = raw_input("continue with this dosage(y) or enter new dosage(n) >").lower()
+
+            if procede == 'y':
+                break
+            else:
+                continue
+        else:
+            break
+
+
+    numOfDays = raw_input("Enter the number of day for the medication >")
+
+    daymodifer = str('+'+str(numOfDays)+' days')
+
+    # (hcno, chart_id, staff_id, mdate, start_med, end_med, amount, drug_name)
+    cursor.execute('''
+        insert into medications values (?,?,?,datetime('now'),datetime('now'),datetime('now', ?),?,?);
+        ''', (hcno, chart_id, staff_id, daymodifer, amount, medication, ))
+    conn.commit()
 
